04fa7473d12a618ec3d3303950a599375b2267c7
NataSerg <108566394+NataSerg@users.noreply.github.com>
2022-09-12 16:51:12 +0300

    Add files via upload

---
+++ hw9-pagination/app.js
@@ -0,0 +1,96 @@
+function createPagination(data) {
+    let count = data.total / data.limit;
+    let htmlStr = '';
+    const pageNumber = (data.skip / 10) + 1;
+    for (item = 0; item < count; item++) {
+        htmlStr += `<li class="page-item ${pageNumber === item + 1 ? 'active' : ''}">
+        <span class="page-link">${+item + 1}</span></li>`;
+    }
+    document.querySelector('.pagination').innerHTML = htmlStr;
+    
+}
+
+function renderProducts(data) {
+    const { products } = data;
+    console.log(data);
+    
+
+    const htmlStr = products.map(product => `<div class="card col-sm-6 col-md-4 col-lg-3">
+                <img src="${product.thumbnail}" class="card-img-top" alt="${product.brand} ${product.title}">
+                <div class="card-body">
+                    <h5 class="card-title">${product.brand} ${product.title}</h5>
+                    <p class="card-text">${product.description}</p>
+                    <a href="#" class="btn btn-primary">Add to Cart</a>
+                </div>
+                </div>`).join('');
+    
+    document.getElementById('products').innerHTML = htmlStr;
+
+    createPagination(data);
+}
+
+function getProductsByCategory(categoryName) {
+    let url = 'https://dummyjson.com/products?limit=10';
+    if (categoryName) { url = `https://dummyjson.com/products/category/${categoryName}`;}
+    fetch(url).then(res => res.json()).then(data => {
+        renderProducts(data);
+    })
+
+}
+
+function getNewProductsPortion(skip) {
+    fetch('https://dummyjson.com/products?limit=10&skip=' + skip).then(res => res.json()).then(data =>renderProducts(data))
+
+}
+
+function setListeners() {
+    document.getElementById('categories').onchange = e => {
+        getProductsByCategory(e.currentTarget.value);
+    }
+    document.querySelector('.pagination').onclick = e => {
+        console.log(e.target);
+        let newPage = e.target.innerText;
+        console.log(newPage);
+        getNewProductsPortion((+newPage - 1) * 10);
+    }
+}
+
+
+function renderCategories(categories) {
+    const categoriesSelect = document.createElement('select');
+    categoriesSelect.className = 'form-control';
+    categoriesSelect.id = 'categories';
+
+    let htmlStr = '<option value="">All</option>';
+    htmlStr += categories.map(category => `<option value="${category}">${category}</option>`).join('');
+    categoriesSelect.innerHTML = htmlStr;
+    document.querySelector('.categories-block').prepend(categoriesSelect);
+    
+    setListeners();
+}
+
+
+window.onload = function () {
+    const productPromise = new Promise((resolve, reject) => {
+        fetch('https://dummyjson.com/products?limit=10').then(res => res.json()).then(data =>resolve(data))
+    })
+
+    const categoriesPromise = new Promise((resolve, reject) => {
+        fetch('https://dummyjson.com/products/categories').then(res => res.json()).then(data =>resolve(data))
+
+    })
+
+    Promise.all([productPromise, categoriesPromise]).then(data => {
+        renderProducts(data[0]);
+        renderCategories(data[1]);
+    })
+}
+
+
+
+
+
+
+
+
+
